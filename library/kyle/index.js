953cb2b0d3709714771741279b36a6f2ff734dd7
kyle <kyledr215@gmail.com>
2018-10-09 14:35:09 -0400

    pokemon data

--- library/kyle/index.js
+++ library/kyle/index.js
@@ -4,64 +4,106 @@
 // defense - integer
 // abilities - array of strings
 
+var array = [395, 493, 6]
+var counter = 0;
+pokemonDisplay = document.getElementById('pokemonImg');
+buttonRight = document.getElementById('button-right')
+buttonLeft = document.getElementById('button-left')
+
+/* -------------------------------------------- button function ----------------------------------------------------------*/
+buttonRight.addEventListener('click', function(){
+  counter += 1
+  if(counter > 2){
+    counter = 0;
+  }
+  ajaxWrapper(array[counter])
+})
+
+buttonLeft.addEventListener('click', function(){
+counter -= 1
+  if(counter < 0){
+    counter = array.length-1
+  }
+ajaxWrapper2(array[counter])
+})
+
+
+/* -------------------------------------------- classes ----------------------------------------------------------*/
 
 class Pokemon{
-  constructor(result){
-  this.name = result.name;
-  this.number = result.id;
-  this.hp = result.stats[5].base_stat;
-  this.attack = result.stats[4].base_stat;
-  this.defense = result.stats[3].base_stat;
+  constructor(name, number, hp, id, attack, defense, sprite, abilities){
+  this.name = name;
+  this.number = id;
+  this.hp = hp;
+  this.attack = attack;
+  this.defense = defense;
   this.abilities = [];
   }
-
-  for(let i = 0; i < results.)
 }
 
 class Trainer{
-  constructor(name){
+  constructor(name, picture){
     this.name = name;
+    this.picture = picture;
   }
-
 }
 
+function pokemonParser(result){
+  // console.log(result)
+  pokemon = new Pokemon()
+  pokemon.name = result.name;
+  pokemon.number = result.id;
+  pokemon.hp = result.stats[5].base_stat;
+  pokemon.attack = result.stats[4].base_stat;
+  pokemon.defense = result.stats[3].base_stat;
+  pokemon.sprite = result.sprites.front_default;
+  pokemon.abilities = result.abilities;
 
+
+  formating(pokemon)
+}
+
+/* -------------------------------------------- formatting  ----------------------------------------------------------*/
+
+function formating(pokemon){
+  pokemonImg.style.height = '200px';
+  pokemonImg.style.width = '200px';
+  pokemonImg.style.backgroundImage = "url(" + pokemon.sprite + ")"
+  pokemonImg.style.backgroundSize = "100% 100%"
+  // console.log(pokemon.sprite)
+}
 
 
 
 /* -------------------------------------------- Ajax calls ----------------------------------------------------------*/
+console.log(array[0])
 
-function ajax1(){
-  $.ajax({url: "https://pokeapi.co/api/v2/pokemon/395/",
+  $.ajax({url: "https://pokeapi.co/api/v2/pokemon/" + array[0] + "/",
           success: function(result){
           // console.log(result)
-          console.log(result)
-          let empoleon = new Pokemon(result)
-          console.log(empoleon.name)
+          pokemonParser(result)
         }
   });
 
-}
-
-function ajax2(){
-        $.ajax({url: "https://pokeapi.co/api/v2/pokemon/493/",
+function ajaxWrapper(x) {
+    
+        $.ajax({url: "https://pokeapi.co/api/v2/pokemon/"+ x  +"/",
                 success: function(result){
                 // console.log(result)
-
-                console.log(result)
+                pokemonParser(result)
             }
           });
-      }
-function ajax3(){
-  $.ajax({url: "https://pokeapi.co/api/v2/pokemon/6/",
+}
+
+function ajaxWrapper2(x){
+  $.ajax({url: "https://pokeapi.co/api/v2/pokemon/" + x + "/",
           success: function(result){
           // console.log(result)
-          console.log(result)
+          pokemonParser(result)
       }});
 }
 
-window.onload = function(){
-  ajax1()
-  ajax2()
-  ajax3()
-}
+// window.onload = function(){
+//  ajaxWrapper(array)
+//
+// }
